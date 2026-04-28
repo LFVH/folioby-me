e03c725fcb03a20a7e797c233cf07689abcf8045
LFVH <luizfelipevh@gmail.com>
2026-04-27 22:48:59 -0300

    ativando tudo

--- src/app/api/auth/auth/definitions.ts
+++ src/app/api/auth/auth/definitions.ts
@@ -4,6 +4,7 @@ export const SignupFormSchema = z.object({
   name: z
     .string()
     .min(2, { message: 'Name must be at least 2 characters long.' })
+    .max(100, { message: 'Name is too long.' })
     .trim(),
   email: z.string().email({ message: 'Please enter a valid email.' }).trim().transform(email => email.toLowerCase()),
   password: z
