35cc60c0892cdf9ac1e720b420e3228c1a7bb9a3
LFVH <luizfelipevh@gmail.com>
2026-09-11 03:21:04 -0400

    more ui changes

--- src/components/signinsignup/icons.tsx
+++ src/components/signinsignup/icons.tsx
@@ -144,3 +144,21 @@ export function HomeIcon(props: React.SVGProps<SVGSVGElement>) {
       </svg>
     );
   }
+
+  export function MoreDotsIcon(props: React.SVGProps<SVGSVGElement>) {
+    return (
+      <svg
+        {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        aria-hidden="true"
+      >
+        <circle cx="5" cy="12" r="1.8" />
+        <circle cx="12" cy="12" r="1.8" />
+        <circle cx="19" cy="12" r="1.8" />
+      </svg>
+    );
+  }
